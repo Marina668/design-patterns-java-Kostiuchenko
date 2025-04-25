abfa6d884426a555a7107c7d88befe6a89692b0a
Maryna Kostiuchenko <marina.k.kost@gmail.com>
2025-04-25 23:26:51 +0300

    lab 14 task 3_3

--- Java/lab14_Chain/task_3_3/src/Client.java
+++ Java/lab14_Chain/task_3_3/src/Client.java
@@ -1,9 +1,26 @@
-
+import java.util.Random;
 
 public class Client {
     public static void main(String[] args) {
 
-        System.out.println("Hello!");
+        Handler additionHandler = new AddHandler();
+        Handler substractionHandler = new SubHandler();
+        Handler multiplicationHandler = new MultHandler();
+        Handler divisionHandler = new DivHandler();
+
+        additionHandler.setNext(substractionHandler);
+        substractionHandler.setNext(multiplicationHandler);
+        multiplicationHandler.setNext(divisionHandler);
+
+        Random random = new Random();
+        String[] mathOperations = {"+", "-", ":", "*"};
+        for (int i = 0; i < 20; i++) {
+            int leftNumber = random.nextInt(20);
+            int rightNumber = random.nextInt(5);
+            String operation = mathOperations[random.nextInt(mathOperations.length)];
+            MathExpression mathExpression = new MathExpression(leftNumber, rightNumber, operation);
+            additionHandler.calculate(mathExpression);
+        }
 
 
     }
